d4ce3db082bedde3240db7fe043cd7bfbc0e6831
Yaman <husoyaman1987@gmail.com>
2021-04-19 00:07:31 +0300

    Bir Listedeki Sayı Dizinin En Çok Tekrar Edeni ve Sayısı

--- Assignment_6.py
+++ Assignment_6.py
@@ -1,4 +1,8 @@
-numbers = [1, 3, 7, 4, 3, 0, 3, 6, 3]
+numbers = [1, 3, 7, 4, 3, 0, 6, 3, 7 ,8 ,4 ,4 ,4 ,6 ,7 ,6 ,5 ,4, 5, 4, ]
+harf = ["a", "g", "h", "k", "l", "m", "f" , "d" , "s" ]
+print(max(numbers))
+print(max(harf))
+print(numbers.count(3))
 
 most_frequent = max(numbers, key = numbers.count) # Burada max() fonksiyonu keyi olarak count belirledik, ve listenin içinde en çok geçen elemanı verir
                                                   # count() fonksiyonu da bir list fonksiyonu olduğu için list ismi ile birlikte belirtilmelidir. len() fonksiyonunu key için kullanırken böyle birşey yok
